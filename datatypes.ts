3cc87b125eb32f603695e55e8f65d2a2ab08c006
rxmxndai <k.romann413@gmail.com>
2023-07-22 13:10:18 +0545

    Basic datatypes and TS variable defining

---
+++ datatypes.ts
@@ -0,0 +1,21 @@
+let myName: string = "Roman Karki";
+let meaningOfLife: number;
+let isLoading: boolean;
+let album: any;
+let re: RegExp = /\w+/g;
+// union with datatype | datatype
+
+meaningOfLife = 42;
+isLoading = true;
+album = 515;
+
+
+const sum = (a: string, b: string) => {
+    return a+b;
+}
+
+let postId: string | number;
+let isActive: number | boolean;
+
+
+
